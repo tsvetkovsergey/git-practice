9a250a182900335c236fb268a0464d34d834cff0
Sergei Tsvetkov <tsvetkov2004@gmail.com>
2024-11-11 00:17:44 +0300

    add Pink Floyd lyrics

--- src/components/UserBox/UserBox.jsx
+++ src/components/UserBox/UserBox.jsx
@@ -5,7 +5,13 @@ export const UserBox = () => {
   return (
     <div className="box">
       <h3 className="authorname">Sergei Tsvetkov</h3>
-      <p>Hello</p>
+      <p>
+        {`So, so you think you can tell heaven from hell?
+        Blue skies from pain?
+        Can you tell a green field from a cold steel rail?
+        A smile from a veil?
+        Do you think you can tell?`}
+      </p>
       <p className="date">18.11.2024</p>
     </div>
   );
