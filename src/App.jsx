1d97836d863ebe2b36504b54010581609debfa0b
Sergei Tsvetkov <tsvetkov2004@gmail.com>
2024-11-10 23:57:40 +0300

    add user box

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import { UserBox } from './components/UserBox/UserBox';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <div>
+        <UserBox />
         <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         <button onClick={() => setCount(count + 1)}>{count}</button>
       </div>
---
+++ src/components/UserBox/UserBox.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+import './UserBox.styles.css';
+
+export const UserBox = () => {
+  return (
+    <div className="box">
+      <p>Hello</p>
+    </div>
+  );
+};
