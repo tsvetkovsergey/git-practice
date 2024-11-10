4ddd87096991e4770488801b791ca105e934139d
Sergei Tsvetkov <tsvetkov2004@gmail.com>
2024-11-11 00:03:35 +0300

    add author name

--- src/components/UserBox/UserBox.jsx
+++ src/components/UserBox/UserBox.jsx
@@ -4,6 +4,7 @@ import './UserBox.styles.css';
 export const UserBox = () => {
   return (
     <div className="box">
+      <h3 className="authorname">Sergei Tsvetkov</h3>
       <p>Hello</p>
     </div>
   );
