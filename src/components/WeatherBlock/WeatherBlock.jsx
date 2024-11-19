1260d89cd118269c45be3294690f4b709b4a38ac
Sergei Tsvetkov <tsvetkov2004@gmail.com>
2024-11-19 13:49:37 +0300

    The weather forecast

--- src/components/WeatherBlock/WeatherBlock.jsx
+++ src/components/WeatherBlock/WeatherBlock.jsx
@@ -3,7 +3,10 @@ import Block from '../Block/Block';
 export default function WeatherBlock() {
   return (
     <Block styles={{ backgroundColor: 'cornflowerblue', margin: '1rem 0' }}>
-      <div>Today is cloudy and rainy. Temperature is 5 degrees Celsius.</div>
+      <div>
+        Today is cloudy and rainy. Temperature is 5 degrees Celsius. Wear your
+        coat!
+      </div>
     </Block>
   );
 }
